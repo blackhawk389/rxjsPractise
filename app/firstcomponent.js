8c9a31e44d7d52a54922a5bf0fc18cb9190ef373
sarah <sarah.hassan95@yahoo.com>
2016-07-14 22:09:32 +0500

    Subject as proxy between components

--- app/firstcomponent.js
+++ app/firstcomponent.js
@@ -12,7 +12,36 @@ var core_1 = require('@angular/core');
 var Observable_1 = require('rxjs/Observable');
 require('rxjs/add/observable/from');
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var Subject_1 = require('rxjs/Subject');
 //keyup and enter key event binding
+var secondClass = (function () {
+    function secondClass() {
+        this.localarray = [];
+        //    this.proxySubject.subscribe(function(data){
+        //        this.localarray.push(data);
+        //   }) 
+    }
+    secondClass.prototype.ngOnInit = function () {
+        var _this = this;
+        //cold observer sending values from start
+        this.sendingSubject.subscribe(function (data) {
+            _this.localarray.push(data);
+        });
+    };
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Subject_1.Subject)
+    ], secondClass.prototype, "sendingSubject", void 0);
+    secondClass = __decorate([
+        core_1.Component({
+            selector: 'second',
+            template: "\n    \n    <div>  \n    <div *ngFor = \"let x of localarray\">{{x}} </div>\n    </div>   \n    "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], secondClass);
+    return secondClass;
+}());
+exports.secondClass = secondClass;
 var first = (function () {
     //proxySubject = Observable.from(this.array);
     //  1. You are subscribing everytime the button clicked, you should subscribe once 2. Your first subscribe comes after your first next, that's why you miss the first value. 3. call .asObservable where you pass it to another component (i.e pass proxySubject.asObservable()). Just calling it alone has no effect, it's a function and you don't do anything with the return value
@@ -22,11 +51,11 @@ var first = (function () {
             next: function (v) { return console.log(v); },
             complete: function () { return console.log("complete"); }
         };
-        //this.proxySubject = new Subject();
+        this.proxySubject = new Subject_1.Subject();
         //how to subscribe to the observable this can also done with simple
         //variable, lets not do this
         // this.proxySubject = Observable.from(this.array);
-        Observable_1.Observable.from(this.array).subscribe(this.observer);
+        Observable_1.Observable.from(this.array).subscribe(this.proxySubject.asObservable);
         //this.proxySubject.subscribe(this.observer
         //            function(data){
         //       //if there is conosle with string all values will be string
@@ -41,8 +70,9 @@ var first = (function () {
     }
     first.prototype.addto = function () {
         this.array.push(this.inputValue);
-        // this.proxySubject.next(this.array);
-        this.observer.next(this.array);
+        //this.proxySubject.next(this.array)
+        this.proxySubject.next(this.inputValue);
+        this.observer.next(this.inputValue);
         //console.log(this.array);
         //     this.proxySubject.subscribe(function(data){
         //    console.log(data)
@@ -54,7 +84,8 @@ var first = (function () {
     first = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n            <input type=\"text\" required \n            [(ngModel)]=\"inputValue\" name=\"inputValue\" >  \n            <button (click)=\"addto()\">submit</button>  \n               \n        "
+            template: "\n            <input type=\"text\" required \n            [(ngModel)]=\"inputValue\" name=\"inputValue\" >  \n            <button (click)=\"addto()\">submit</button>\n            <second [sendingSubject]=\"proxySubject\"></second>               \n        ",
+            directives: [secondClass]
         }), 
         __metadata('design:paramtypes', [])
     ], first);
--- app/firstcomponent.ts
+++ app/firstcomponent.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
 import { bootstrap }    from '@angular/platform-browser-dynamic';
@@ -7,15 +7,58 @@ import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 
 //keyup and enter key event binding
+
+@Component({
+    selector : 'second',
+    template : `
+    
+    <div>  
+    <div *ngFor = "let x of localarray">{{x}} </div>
+    </div>   
+    `
+})
+export class secondClass implements OnInit{
+    
+    localarray : any[];
+    data;
+    @Input() sendingSubject : Subject<any>
+    
+    constructor(){
+        
+        this.localarray = [];
+        
+       
+    //    this.proxySubject.subscribe(function(data){
+    //        this.localarray.push(data);
+    //   }) 
+    }
+    
+    ngOnInit(){
+        //cold observer sending values from start
+           this.sendingSubject.subscribe((data)=>{
+               this.localarray.push(data)
+           }
+      ) 
+        
+    }
+     
+    
+    
+}
+
+
+
+
 @Component(
     {
         selector: 'my-app',
         template: `
             <input type="text" required 
             [(ngModel)]="inputValue" name="inputValue" >  
-            <button (click)="addto()">submit</button>  
-               
-        `
+            <button (click)="addto()">submit</button>
+            <second [sendingSubject]="proxySubject"></second>               
+        `,
+        directives : [secondClass]
     }
 )
 export class first{
@@ -40,16 +83,11 @@ export class first{
         
       
         
-     //this.proxySubject = new Subject();
+     this.proxySubject = new Subject();
      //how to subscribe to the observable this can also done with simple
      //variable, lets not do this
    // this.proxySubject = Observable.from(this.array);
-       Observable.from(this.array).subscribe(this.observer
-        //function(data){
-          
-    //        console.log("from observable "+ data)
-       //}
-       )
+       Observable.from(this.array).subscribe(this.proxySubject.asObservable)
        //this.proxySubject.subscribe(this.observer
     //            function(data){
     //       //if there is conosle with string all values will be string
@@ -67,8 +105,9 @@ export class first{
        
        
         this.array.push(this.inputValue)
-       // this.proxySubject.next(this.array);
-       this.observer.next(this.array);
+        //this.proxySubject.next(this.array)
+        this.proxySubject.next(this.inputValue);
+       this.observer.next(this.inputValue);
         //console.log(this.array);
     //     this.proxySubject.subscribe(function(data){
     //    console.log(data)
@@ -80,4 +119,6 @@ export class first{
       
     }
 }
+
+
 bootstrap(first);
